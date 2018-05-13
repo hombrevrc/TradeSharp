28d3c86515dcfe1bc7fe8215a8652b14a11a43c6
Vladimir <vladimir@mail.com>
2018-05-13 22:22:31 +0300

    Add file for AccountInfoCommentScript

---
+++ src/TradeSharp.Client/BL/Script/AccountInfoCommentScript.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Candlechart;
+using Candlechart.ChartMath;
+
+namespace TradeSharp.Client.BL.Script
+{
+    public class AccountInfoCommentScript : TerminalScript
+    {
+        public override string ActivateScript(CandleChartControl chart, PointD worldCoords)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ActivateScript(string ticker)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ActivateScript(bool byTrigger)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
