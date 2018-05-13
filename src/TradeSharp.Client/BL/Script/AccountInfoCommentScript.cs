6ae1a9fa8555af447abd885ce8973b5894678d44
Vladimir <vladimir@mail.com>
2018-05-13 23:12:20 +0300

    minor logic for AccountInfoCommentScript

--- src/TradeSharp.Client/BL/Script/AccountInfoCommentScript.cs
+++ src/TradeSharp.Client/BL/Script/AccountInfoCommentScript.cs
@@ -5,11 +5,22 @@ using System.Text;
 using System.Threading.Tasks;
 using Candlechart;
 using Candlechart.ChartMath;
+using TradeSharp.Client.Util;
+using TradeSharp.Contract.Entity;
+using TradeSharp.Util;
 
 namespace TradeSharp.Client.BL.Script
 {
+    [LocalizedDisplayName("TitleAccountInfoCommentScript")]
     public class AccountInfoCommentScript : TerminalScript
     {
+        private const string CommentSpecName = "AccountInfo";
+
+        public AccountInfoCommentScript()
+        {
+            ScriptName = Localizer.GetString("TitleAccountInfoCommentScript"); ;
+        }
+
         public override string ActivateScript(CandleChartControl chart, PointD worldCoords)
         {
             throw new NotImplementedException();
@@ -17,12 +28,37 @@ namespace TradeSharp.Client.BL.Script
 
         public override string ActivateScript(string ticker)
         {
-            throw new NotImplementedException();
+            throw new Exception("Неверный тип вызова скрипта \"AccountInfoCommentScript\"");
         }
 
         public override string ActivateScript(bool byTrigger)
         {
-            throw new NotImplementedException();
+            if (!byTrigger)
+                throw new Exception("Неверный тип вызова скрипта \"AccountInfoCommentScript\"");
+
+            var accountData = AccountStatus.Instance.AccountData;
+            var scriptText = GetCommentText(accountData);
+
+            // обновить комментарии на графиках
+            var charts = MainForm.Instance.GetChartList(true);
+            foreach (var chart in charts)
+            {
+                var comment = chart.seriesComment.data.FirstOrDefault(c => c.Name == CommentSpecName);
+                if (comment == null) continue;
+
+                comment.Text = scriptText;
+            }
+
+            return string.Empty;
+        }
+
+        /// <summary>
+        /// формируем текст для коментария
+        /// </summary>
+        private string GetCommentText(Account accountData)
+        {
+            var scriptText = $"[b] Balance {accountData.Balance}";
+            return scriptText;
         }
     }
 }
