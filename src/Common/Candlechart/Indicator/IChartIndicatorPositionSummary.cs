6e434d4e8ff5f47f5685a1f1e98dc0318264fd12
Vladimir <vladimir@mail.com>
2018-07-19 20:49:47 +0300

    Add PositionSummary to IndicatorOder

--- src/Common/Candlechart/Indicator/IChartIndicatorPositionSummary.cs
+++ src/Common/Candlechart/Indicator/IChartIndicatorPositionSummary.cs
@@ -1,7 +1,10 @@
-﻿namespace Candlechart.Indicator
+﻿using System.Collections.Generic;
+using TradeSharp.Contract.Entity;
+
+namespace Candlechart.Indicator
 {
     public interface IChartIndicatorPositionSummary
     {
-        void UpdatePositionSummary(float profit, float profitInPercent);
+        void UpdatePositionSummary(float totalProfit, float totalProfitInPercent, List<MarketOrder> openOrders);
     }
 }
